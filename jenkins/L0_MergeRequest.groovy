27e3d02ea91aac38c1b1259555433759c0f7acfb
ZhanruiSunCh <184402041+ZhanruiSunCh@users.noreply.github.com>
2025-03-20 10:02:48 +0000

    Test reuse-pipeline

--- jenkins/L0_MergeRequest.groovy
+++ jenkins/L0_MergeRequest.groovy
@@ -1025,6 +1025,7 @@ def launchStages(pipeline, reuseBuild, testFilter, enableFailFast, disableIncrem
 
                 def stageName = "Build"
                 stage(stageName) {
+                    return
                     def parameters = getCommonParameters()
                     parameters += [
                         'enableFailFast': enableFailFast,
@@ -1103,7 +1104,7 @@ def launchStages(pipeline, reuseBuild, testFilter, enableFailFast, disableIncrem
         },
     ]
 
-    if (windowsPreMerge && testFilter[IS_POST_MERGE]) {
+    if (windowsPreMerge && testFilter[IS_POST_MERGE] && false) {
         def stageNamePreMerge = "Pre-Merge-Windows"
         stages += [
             (stageNamePreMerge): {
@@ -1237,13 +1238,7 @@ pipeline {
                             }
                         }
                     } else {
-                        //launchStages(this, reuseBuild, testFilter, enableFailFast, disableIncrementalBuild, enablePublishLastKnownGood)
-                        stage("Release Check") {
-                            script {
-                                echo "Only run Release Check to keep the build status green"
-                                launchReleaseCheck(this)
-                            }
-                        }
+                        launchStages(this, reuseBuild, testFilter, enableFailFast, disableIncrementalBuild, enablePublishLastKnownGood)
                     }
                 }
             }
