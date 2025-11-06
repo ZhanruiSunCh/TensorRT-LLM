3c2487124445d0903a44081b9d6cb4196879a3c9
ZhanruiSunCh <184402041+ZhanruiSunCh@users.noreply.github.com>
2025-11-06 01:25:09 -0800

    Test
    Signed-off-by: ZhanruiSunCh <184402041+ZhanruiSunCh@users.noreply.github.com>

--- jenkins/Build.groovy
+++ jenkins/Build.groovy
@@ -1,4 +1,4 @@
-@Library(['bloom-jenkins-shared-lib@main', 'trtllm-jenkins-shared-lib@main']) _
+@Library(['bloom-jenkins-shared-lib@main', 'trtllm-jenkins-shared-lib@user/zhanruis/jira_9100']) _
 
 import groovy.transform.Field
 
